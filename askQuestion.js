a4d4d13639eeec9c14f75eced8d1698c7af171ae
codesmitty <varcodesmitty@gmail.com>
2020-10-19 21:31:34 -0400

    Added mailto: method to question form, waiting on future edits to commit to main page

--- askQuestion.js
+++ askQuestion.js
@@ -2,23 +2,23 @@ const width = document.body.clientWidth;
 const formContainer = document.getElementById("form-container");
 
 const formData = {
-    subject:"",
-    body:""
-}
+  subject: "",
+  body: "",
+};
 
 const onResize = () => {
   const newWidth = (width.textContent = window.innerWidth);
 
-if(newWidth > 1030){
-    formContainer.style.background = 'transparent'
-}else if(newWidth > 1024){
-     formContainer.style.background =
-     "radial-gradient(rgba(60, 60, 60, .2), rgba(40, 40, 40, .7))";
-}else if (newWidth < 1024) {
+  if (newWidth > 1030) {
+    formContainer.style.background = "transparent";
+  } else if (newWidth > 1024) {
+    formContainer.style.background =
+      "radial-gradient(rgba(60, 60, 60, .2), rgba(40, 40, 40, .7))";
+  } else if (newWidth < 1024) {
     formContainer.style.background =
       "radial-gradient(rgba(20, 20, 20, .5), rgb(0, 0, 0))";
   }
-}
+};
 window.addEventListener("resize", onResize);
 
 const subjectInput = (event) => {
@@ -26,23 +26,26 @@ const subjectInput = (event) => {
   return log;
 };
 
-
 const subject = document.getElementById("title");
-const textBox = document.getElementById('text-box');
+const textBox = document.getElementById("text-box");
 
+document.getElementById("form-btn").addEventListener("click", () => {
+  let message = {
+    subject: "",
+    content: "",
+  };
 
-document.getElementById('form').addEventListener("submit", event =>{
-    event.preventDefault()
-    let message = {
-        subject:"",
-        content:''
-    }
-    
-    message.subject = subject.value;
-    message.content = textBox.value;
-
-    console.log(message)
-})
-
-
-
+  console.log("hello");
+  message.subject = subject.value;
+  message.content = textBox.value;
+  document
+    .getElementById("form")
+    .setAttribute(
+      "action",
+      "mailto:varcodesmitty@gmail.com?subject=" +
+        message.subject +
+        "&body=" +
+        message.content
+    );
+  document.getElementById("form").submit();
+});
